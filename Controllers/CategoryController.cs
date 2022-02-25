08c3022dd6978b44eb22e8c2ac9970a477c29bb0
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-02-25 17:59:50 +0800

    add index action

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,14 +4,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication17.Data;
+using WebApplication17.Models;
 
 namespace WebApplication17.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> result = _db.Categories.ToList();
+            return View(result);
         }
     }
 }
