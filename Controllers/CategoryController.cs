4d7fdbd5208e4287c71059ed42c24e76eb45a687
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-02-26 14:36:44 +0800

    Edit function

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -54,6 +54,25 @@ namespace WebApplication17.Controllers
         }
 
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null ||id==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var catgoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var catgoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb ==null)
+            {
+                return NotFound();
+            }
+
+
+            return View(categoryFromDb);
+        }
+
 
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,7 +15,8 @@ namespace WebApplication17.Models
 
         public string Name { get; set; }
 
-
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 and 100 only")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreateDateTime { get;set;} = DateTime.Now;
